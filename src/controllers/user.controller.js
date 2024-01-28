e6e4f0802b0cc63cb368359dd5364840b56ca630
CoderNandan <nandanindia1947@gmail.com>
2024-01-28 21:07:35 +0530

    all router is sad

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -499,4 +499,5 @@ export {
   updateUserCoverImage,
   getUserChannlProfile,
   getUserChannlProfile,
+  getWatchHistory,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannlProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updatAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { varifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,5 +37,16 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(varifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(varifyJWT, changeCurrentPassword);
+router.route("current-user").get(varifyJWT, getCurrentUser);
+router.route("/update-acount").patch(varifyJWT, updatAccountDetails);
+router
+  .route("/update-avater")
+  .post(varifyJWT, upload.single("avater"), updateUserAvatar);
+router
+  .route("/update-coverImage")
+  .post(varifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/channel/:userName").get(varifyJWT, getUserChannlProfile);
+router.route("/weatchHistory").get(varifyJWT, getWatchHistory);
 
 export default router;
